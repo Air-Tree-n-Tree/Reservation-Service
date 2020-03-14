7a930a5cf5ae4ac698b4940a143d1a600fc7f3d0
Jin <jinruncai@gmail.com>
2020-03-14 12:48:38 -0700

    Refactor seeding utilites to generate unbiased reservation lengths.
    Generating a random range biased reservation lengths against using
    the maximum length (maxNights). The result was that lengths were distributed
    such that most were shorter than the median (maxNights - minNights).

--- database/utils/random.js
+++ database/utils/random.js
@@ -1,23 +1,13 @@
 
+/**
+ * @param {Number} min Lower bound, inclusive
+ * @param {Number} max Upper bound, inclusive
+ */
 const valueInRange = (min, max) => (
   // The maximum is exclusive and the minimum is inclusive
   min + Math.floor(Math.random() * (max - min))
 );
 
-/**
- * Generates a random range within the given range.
- * @param {Number} min The minimum value of the range's lower bound, inclusive.
- * @param {Number} max The maximum value of the range's upper bound, exclusive.
- * @param {Number} [minLength] The minimum length of the range.
- * @returns {Array<Number>} Tuple: [lowerbound, upperbound, length]
- */
-const range = (min, max, minLength = 1) => {
-  const lowerBound = valueInRange(min, max - minLength);
-  const upperBound = valueInRange(lowerBound + minLength, max);
-  return [lowerBound, upperBound, upperBound - lowerBound];
-};
-
 module.exports = {
-  range,
   valueInRange,
 };
