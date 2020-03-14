aff0854868177735c2108d7967411239c5fcfc33
Jin <jinruncai@gmail.com>
2020-03-13 20:31:37 -0700

    Create database seeding script

---
+++ database/seed.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+const Availability = require('./models/Availability');
+const generateAvailability = require('./utils/generateAvailability');
+
+mongoose.connect('mongodb://localhost/room-reservations', (err) => {
+  if (err) {
+    throw new Error('Failed to connect to mongo database: room-reservations');
+  }
+  console.log('Connected to mongo database: room-reservations');
+});
+
+const seedDatabase = () => {
+  const seeds = [];
+  for (let i = 0; i < 100; i += 1) {
+    seeds.push(new Promise((resolve, reject) => {
+      generateAvailability(i).save()
+        .then(resolve)
+        .catch(reject);
+    }));
+  }
+  return Promise.all(seeds);
+};
+
+Availability.remove({}) // Clear the database
+  .then(seedDatabase)
+  .then(() => {
+    console.log('Successfully seeded database');
+  })
+  .catch(console.err)
+  .then(mongoose.disconnect);
