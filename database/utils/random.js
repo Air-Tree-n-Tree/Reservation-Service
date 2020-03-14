1f7562201ca7f9580e94c5cf1bfec17a607df60b
Jin <jinruncai@gmail.com>
2020-03-13 18:29:09 -0700

    Create utility functions for seeding random data

---
+++ database/utils/random.js
@@ -0,0 +1,23 @@
+/**
+ * Generates a random range within the given range.
+ * @param {Number} min The minimum value of the range's lower bound, inclusive.
+ * @param {Number} max The maximum value of the range's upper bound, exclusive.
+ * @param {Number} [minLength] The minimum length of the range.
+ * @returns {Array<Number>} Tuple: [lowerbound, upperbound, length]
+ */
+
+const randomValueInRange = (min, max) => (
+  // The maximum is exclusive and the minimum is inclusive
+  min + Math.floor(Math.random() * (max - min))
+);
+
+const generateRandomRange = (min, max, minLength = 1) => {
+  const lowerBound = randomValueInRange(min, max - minLength);
+  const upperBound = randomValueInRange(lowerBound, max);
+  return [lowerBound, upperBound, upperBound - lowerBound];
+};
+
+module.exports = {
+  generateRandomRange,
+  randomValueInRange,
+};
