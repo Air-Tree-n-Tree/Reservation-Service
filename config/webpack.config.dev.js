1be93b8012e1851d06553a3b38dc67b1a3fe9e1a
Jin <jinruncai@gmail.com>
2020-03-17 16:02:50 -0700

    Add HtmlWebpackPlugin to serve templates for development

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -1,10 +1,16 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { ROOT, CLIENT } = require('../constants/PATHS');
 
 module.exports = {
   mode: 'development',
   context: path.resolve(CLIENT),
   entry: path.resolve(CLIENT, 'index.jsx'),
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(CLIENT, 'templates', 'index.html'),
+    }),
+  ],
   output: {
     filename: 'bundle.js',
     path: path.resolve(ROOT, 'public'),
