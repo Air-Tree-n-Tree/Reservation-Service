ef6751bd55db9971bec9db63465f36a40a24f6fe
Jin <jinruncai@gmail.com>
2020-03-16 23:59:57 -0700

    Add jest-enzyme library. Configure jest setup.
    Setup enzyme library and adapters after jest env.

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,13 @@
+const path = require('path');
+const ROOT_DIR = require('./constants/ROOT_DIR');
+
 module.exports = {
   clearMocks: true, // Automatically clear mock calls and instances before every test.
   collectCoverage: true,
   coverageDirectory: 'coverage',
   testEnvironment: 'node',
+  setupFilesAfterEnv: [
+    './node_modules/jest-enzyme/lib/index.js', // Setup jest-enzyme library
+    path.resolve(ROOT_DIR, 'tests', 'setupTests.js'), // Configure enzyme react adapter
+  ],
 };
