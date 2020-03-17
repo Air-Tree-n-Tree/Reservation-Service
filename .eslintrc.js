f09269f8fac237ef07b88e589f236e7ffd8dc1bf
Jin <jinruncai@gmail.com>
2020-03-16 17:05:23 -0700

    Revert "Add no-console rule."
    This reverts commit af95f956f195a06c791b73518e8e4e6e16e48497.
    There were too many exceptions to the rule being added to continue
    enforcing no-console, especially since the application is run in
    a node environment.

--- .eslintrc.js
+++ .eslintrc.js
@@ -20,6 +20,6 @@ module.exports = {
         "react"
     ],
     "rules": {
-      "no-console": "error"
-    },
+      "no-console": "off"
+    }
 };
--- index.js
+++ index.js
@@ -2,6 +2,5 @@ const app = require('./server/server');
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
-  /* eslint-disable-next-line no-console */
   console.log('Server listening on: ', port);
 });
