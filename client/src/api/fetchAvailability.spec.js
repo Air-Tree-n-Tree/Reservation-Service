b512f7580cd2ddc315a99f1208e77db122693fa1
Jin <jinruncai@gmail.com>
2020-03-18 11:15:18 -0700

    Add test for fetchAvailability api

---
+++ client/src/api/fetchAvailability.spec.js
@@ -0,0 +1,23 @@
+import fetchAvailability from './fetchAvailability';
+
+test('Should fetch availabilitiy data', (done) => {
+  fetchAvailability(0)
+    .then(({ data }) => {
+      expect(data).toBeDefined();
+      expect(data).toStrictEqual(
+        expect.objectContaining({
+          minNights: expect.any(Number),
+          maxNights: expect.any(Number),
+          minGuests: expect.any(Number),
+          maxGuests: expect.any(Number),
+          maxInfants: expect.any(Number),
+          reservations: expect.any(Array),
+          pricesPerGuests: expect.any(Array),
+        }),
+      );
+      done();
+    })
+    .catch((error) => {
+      done(error);
+    });
+});
