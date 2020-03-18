8661961efe2535bfec1cad2691456bbed7a02ccb
Jin <jinruncai@gmail.com>
2020-03-18 12:06:09 -0700

    Fetch room availability data on mount

--- client/src/containers/Availability.jsx
+++ client/src/containers/Availability.jsx
@@ -1,7 +1,23 @@
-import React from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import fetchAvailability from '../api/fetchAvailability';
 
-const Availability = () => (
-  <div>Availability</div>
-);
+class Availability extends Component {
+  componentDidMount() {
+    const { roomId } = this.props;
+    fetchAvailability(roomId)
+      .then();
+  }
+
+  render() {
+    return (
+      <div>Availability</div>
+    );
+  }
+}
+
+Availability.propTypes = {
+  roomId: PropTypes.oneOfType([PropTypes.string, PropTypes.number]).isRequired,
+};
 
 export default Availability;
