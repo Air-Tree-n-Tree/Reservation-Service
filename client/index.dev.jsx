fd8de03ef899141ef15946e4065a4b181306acc0
Jin <jinruncai@gmail.com>
2020-03-19 12:30:34 -0700

    Fix issue where default export and named export use same name.

--- client/index.dev.jsx
+++ client/index.dev.jsx
@@ -4,14 +4,14 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import store from './src/store/store';
 
-import Availability from './src/containers/Availability';
+import AvailabilityContainer from './src/containers/Availability';
 import PriceSummary from './src/containers/PriceSummary';
 
 const randomRoom = Math.floor(Math.random() * 100);
 
 const availability = (
   <Provider store={store}>
-    <Availability roomId={randomRoom} />
+    <AvailabilityContainer roomId={randomRoom} />
   </Provider>
 );
 const availabilityMount = document.getElementById('availability');
