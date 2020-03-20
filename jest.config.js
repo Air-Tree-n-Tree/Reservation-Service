a7a43734247b60b93a75baea61125673d7060930
Jin <jinruncai@gmail.com>
2020-03-19 23:02:46 -0700

    Add identity-obj-proxy to mock css modules

--- jest.config.js
+++ jest.config.js
@@ -5,6 +5,9 @@ module.exports = {
   clearMocks: true, // Automatically clear mock calls and instances before every test.
   collectCoverage: true,
   coverageDirectory: 'coverage',
+  moduleNameMapper: {
+    '\\.css$': 'identity-obj-proxy',
+  },
   testEnvironment: 'node',
   setupFilesAfterEnv: [
     './node_modules/jest-enzyme/lib/index.js', // Setup jest-enzyme library
