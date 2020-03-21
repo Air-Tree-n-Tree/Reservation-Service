ee93da8c8ef9467673458771f7954c4a6cab3c16
Jin <jinruncai@gmail.com>
2020-03-21 00:06:47 -0700

    Fix bug where days would be stuck on unavailable

--- client/src/store/reducers/selecting.reducer.js
+++ client/src/store/reducers/selecting.reducer.js
@@ -1,5 +1,9 @@
 const selecting = (state = 'checkin', action) => {
   switch (action.type) {
+    case 'CLEAR_CHECKINDAY':
+      return 'checkin';
+    case 'CLEAR_CHECKOUTDAY':
+      return 'checkin';
     case 'SET_CHECKINDAY':
       return 'checkout';
     case 'SET_CHECKOUTDAY':
