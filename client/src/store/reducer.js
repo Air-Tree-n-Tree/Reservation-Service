c356f48c32ab47f274f6622a0b2da24d6493ce3c
Jin <jinruncai@gmail.com>
2020-03-22 15:17:33 -0700

    Use deeper branching reducers

--- client/src/store/reducer.js
+++ client/src/store/reducer.js
@@ -1,18 +1,6 @@
 import { combineReducers } from 'redux';
-import availability from './reducers/availability.reducer';
-import reservedDays from './reducers/reservedDays.reducer';
-import checkInDay from './reducers/checkInDay.reducer';
-import checkOutDay from './reducers/checkOutDay.reducer';
-import currentDay from './reducers/currentDay.reducer';
-import startingDay from './reducers/startingDay.reducer';
-import selecting from './reducers/selecting.reducer';
+import dates from './reducers/dates/dates.reducer';
 
 export default combineReducers({
-  availability,
-  reservedDays,
-  checkInDay,
-  checkOutDay,
-  currentDay,
-  startingDay,
-  selecting,
+  dates,
 });
--- client/src/store/reducers/dates/dates.reducer.js
+++ client/src/store/reducers/dates/dates.reducer.js
@@ -2,11 +2,13 @@ import { combineReducers } from 'redux';
 
 import reservedDates from './reservedDates.reducer';
 import selection from './selection.reducer';
+import constraints from './constraints.reducer';
 
 
 const days = combineReducers({
   reservedDates,
   selection,
+  constraints,
 });
 
 export default days;
