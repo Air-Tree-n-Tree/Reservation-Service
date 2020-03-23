e74251f78a1b9bdad4944a4d7db5fa96efaca535
Jin <jinruncai@gmail.com>
2020-03-23 13:31:34 -0700

    Add prompt for server port in production

--- index.js
+++ index.js
@@ -1,6 +1,26 @@
-const app = require('./server/server');
+/* eslint-disable global-require */
 
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
-  console.log('Server listening on: ', port);
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
 });
+
+const start = function startServer(port) {
+  const app = require('./server/server');
+  app.listen(port, () => {
+    console.log('Server listening on: ', port);
+  });
+};
+
+const port = process.env.PORT || 3002;
+
+if (process.env.NODE_ENV === 'development') {
+  start(3002);
+} else {
+  rl.question('Listen on which port? ', (_port = port) => {
+    start(_port);
+    rl.close();
+  });
+}
