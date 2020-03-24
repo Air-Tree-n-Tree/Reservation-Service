65c7e29e09dc479af75678e547f9ac880dbc313a
Jin <jinruncai@gmail.com>
2020-03-24 16:32:53 -0700

    Serve static files (serve bundle)

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 const db = require('../database');
 
 const app = express();
@@ -6,6 +7,10 @@ const allowedOrigins = [
   'http://localhost:9000',
   'http://localhost:3000',
 ];
+
+app.use(express.static(
+  path.resolve(__dirname, '..', 'public'),
+));
 
 // Allow access from client origin
 app.use('/api/reservations/', (req, res, next) => {
