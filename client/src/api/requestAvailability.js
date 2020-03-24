0e4543925c42fd03cfea9099022d0c11ef8c7941
Jin <jinruncai@gmail.com>
2020-03-23 18:01:40 -0700

    Change default domain to origin

--- client/src/api/requestAvailability.js
+++ client/src/api/requestAvailability.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const domain = process.env.production ? '' : 'http://localhost:3002';
+const domain = process.env.development ? 'http://localhost:3002' : '';
 
 const requestAvailability = (roomId) => (
   axios.get(`${domain}/api/reservations/${roomId}`)
