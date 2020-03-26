1ac6b4783ce11295fff9972a6381cb2fb2a82390
Jin <jinruncai@gmail.com>
2020-03-25 17:06:51 -0700

    Containerize app with Docker

--- database/index.js
+++ database/index.js
@@ -1,11 +1,12 @@
 const mongoose = require('mongoose');
 const Availability = require('./models/Availability');
 
-mongoose.connect('mongodb://localhost/room-reservations', {
+mongoose.connect('mongodb://database:27017/room-reservations', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 }, (err) => {
   if (err) {
+    console.log(err);
     throw new Error('Failed to connect to mongo database: room-reservations');
   }
   console.log('Connected to mongo database: room-reservations');
