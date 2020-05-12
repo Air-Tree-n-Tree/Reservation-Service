596fe20748841212985dddb42e2e3fa1ca0bea6b
Jin Cai <jinruncai@gmail.com>
2020-05-12 15:02:09 -0700

    Change store.prod to prod.store
    While you may think this means nothing. In fact, material icon recognizes prod.store
    as a store, which is plenty sufficient meaning for me.

--- client/src/index.prod.jsx
+++ client/src/index.prod.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import { Provider } from 'react-redux';
-import store from './store/store.prod';
+import store from './store/prod.store';
 
 import './styles/style.css';
 
