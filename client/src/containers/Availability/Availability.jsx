7b877bfee55b666eee7b77d7435f6eea4690537e
Jin Cai <jinruncai@gmail.com>
2020-05-13 11:07:59 -0700

    Remove store connection to root component

--- client/src/containers/Availability/Availability.jsx
+++ client/src/containers/Availability/Availability.jsx
@@ -1,14 +1,12 @@
 import React from 'react';
-import { connect } from 'react-redux';
 
 import CalendarsContainer from '../../components/Calendars/Calendars';
 import Title from '../../components/Title/Title';
 import Caption from '../../components/Caption/Caption';
-import fetchAvailability from '../../store/actions/fetchAvailability.action';
 
 import classes from './Availability.module.css';
 
-export const Availability = () => (
+const Availability = () => (
   <div className={classes.Availability}>
     <Title />
     <Caption />
@@ -16,12 +14,4 @@ export const Availability = () => (
   </div>
 );
 
-const mapStateToProps = ({ dates }) => ({
-  loading: dates.reservedDates === null,
-});
-
-const mapDispatchToProps = {
-  dispatchFetchAvailability: fetchAvailability,
-};
-
-export default connect(mapStateToProps, mapDispatchToProps)(Availability);
+export default Availability;
